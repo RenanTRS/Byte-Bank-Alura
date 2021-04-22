2da685f93ebf40269d7f96d2d8b3610d611f2eba
RenanTRS <renantrsouza@gmail.com>
2021-04-22 13:28:18 -0400

    Teste de de instância

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -1,5 +1,17 @@
 public class CriaConta {
     public static void main(String[] args) {
-        
+        Conta primeiraConta = new Conta(); //Instância do Objeto.        
+        primeiraConta.saldo = 200;
+        System.out.println(primeiraConta.saldo);
+
+        primeiraConta.saldo += 100;
+        System.out.println(primeiraConta.saldo);
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 50;
+        System.out.println(segundaConta.saldo);
+
+        System.out.println("Primeira Conta tem: " +primeiraConta.saldo);
+        System.out.println(("Segunda Conta tem: " +segundaConta.saldo));
     }
 }
