7c3f55b56a9c850773a28517cf68af4d20f6aaf0
RenanTRS <renantrsouza@gmail.com>
2021-04-23 09:42:02 -0400

    Comentários e criação do método saca.

--- src/Conta.java
+++ src/Conta.java
@@ -5,7 +5,10 @@ public class Conta {
     int numero;
     String titular;
 
-    public void deposita(double valor){ //Método
+    public void deposita(double valor){ //Método deposita com void (void para não me informar nada depois de executar.)
         this.saldo += valor; //this opcional, serve apenas para referênciar.
+    }
+    public boolean saca(double valor){ //Boolean vai me retornar se sacou ou não.
+
     }
 }
