87e0acc6bb5e366b549ba45f448eecb0d21c34ee
RenanTRS <renantrsouza@gmail.com>
2021-04-24 12:55:59 -0400

    transferência funcionando

--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -3,16 +3,19 @@ public class TestaMetodo {
         Conta contaPaulo = new Conta();
         contaPaulo.saldo = 100;
         contaPaulo.deposita(50);   //Mandando um valor para o método
-        System.out.println(contaPaulo.saldo);
+        //System.out.println(contaPaulo.saldo);
         
         contaPaulo.saca(20);
-        System.out.println(contaPaulo.saldo);
+        //System.out.println(contaPaulo.saldo);
         contaPaulo.saca(200);
-        System.out.println(contaPaulo.saldo);
+        //System.out.println(contaPaulo.saldo);
 
         Conta contaMarcela = new Conta();
         contaMarcela.deposita(1000);
+        System.out.println(contaMarcela.saldo);
         contaMarcela.transfere(300, contaPaulo);
+        System.out.println(contaMarcela.saldo);
+        System.out.println(contaPaulo.saldo);
 
     }
 }
