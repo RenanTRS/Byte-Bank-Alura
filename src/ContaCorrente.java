933d45b25b24c482b8e1ab4d70165e316c292533
Renan Souza <renantrsouza@gmail.com>
2021-05-07 11:01:48 -0400

    Uso do override

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -2,4 +2,10 @@ public class ContaCorrente extends Conta{
     public ContaCorrente(int agencia, int numero){
         super(agencia, numero);
     }
+    
+    @Override
+    public boolean saca(double valor) {
+        // TODO Auto-generated method stub
+        return super.saca(valor);
+    }
 }
