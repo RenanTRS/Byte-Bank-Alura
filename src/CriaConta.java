888f9695b1f07ba91a08b10b7223bcd58d78963f
Renan Souza <renantrsouza@gmail.com>
2021-05-26 09:21:55 -0400

    update CriaConta.java

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -1,13 +1,6 @@
 public class CriaConta {
     public static void main(String[] args) {
-        //Instancias
-        Conta conta = new Conta(-1135, -243);
-        Cliente cliente = new Cliente();
-
-        //Referência
-        conta.setTitular(cliente);
-        //System.out.println(Conta.getTotal());
-        System.out.println(conta.getAgencia());
-        System.out.println(conta.getNumero());
+        Conta conta = new ContaCorrente(123, 321);
+        
     }
 }
