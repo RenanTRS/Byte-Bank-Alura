18fc78374adaa6c2d93fe1802ee730ea412e4b5e
Renan Souza <renantrsouza@gmail.com>
2021-05-27 09:43:25 -0400

    add tratamento para erro

--- src/TestaSaca.java
+++ src/TestaSaca.java
@@ -3,7 +3,11 @@ public class TestaSaca {
         Conta conta = new ContaCorrente(123, 321);
 
         conta.deposita(200);
-        conta.saca(201);
+        try{
+            conta.saca(201);
+        } catch (SaldoInsuficienteException ex) {
+            System.out.println("Ex: " + ex.getMessage());
+        }
         System.out.println(conta.getSaldo());
 
     }
